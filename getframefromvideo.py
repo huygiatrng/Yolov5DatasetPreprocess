895016e2570a1a453d2d4a8b20221eef4a78b04e
Huy Truong <67343196+huygiatrng@users.noreply.github.com>
2022-08-12 13:10:50 -0400

    Add files via upload

---
+++ getframefromvideo.py
@@ -0,0 +1,17 @@
+import cv2
+
+vidcap = cv2.VideoCapture('input2.mp4')
+success,image = vidcap.read()
+length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+count = 0
+countToName = 0
+while success:
+  if count >=10 :
+    cropped_frame = image[220:860,640:1280]
+    cv2.imwrite("input2 frames/frame%d.jpg" % countToName, cropped_frame)     # save frame as JPEG file
+    print('Progress: '+str(countToName)+"/"+str(int(length/10)))
+    countToName += 1
+    count = 0
+  success, image = vidcap.read()
+  count += 1
+  print(count)
