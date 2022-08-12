bd36cfa98ee938671b67ac5be47056cabbe2762e
Huy Truong <67343196+huygiatrng@users.noreply.github.com>
2022-08-12 13:11:31 -0400

    Update getframefromvideo.py

--- getframefromvideo.py
+++ getframefromvideo.py
@@ -1,13 +1,13 @@
 import cv2
 
-vidcap = cv2.VideoCapture('input2.mp4')
+vidcap = cv2.VideoCapture('inputvideo.mp4')
 success,image = vidcap.read()
 length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 count = 0
 countToName = 0
 while success:
   if count >=10 :
-    cropped_frame = image[220:860,640:1280]
+#     cropped_frame = image[220:860,640:1280] #Optional: Crop frame if needed
     cv2.imwrite("input2 frames/frame%d.jpg" % countToName, cropped_frame)     # save frame as JPEG file
     print('Progress: '+str(countToName)+"/"+str(int(length/10)))
     countToName += 1
